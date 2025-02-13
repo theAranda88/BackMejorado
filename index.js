1e77396acff8480b6a647878ea7ce430dc040de3
Daniel Aranda <thearanda88@gmail.com>
2025-02-13 17:57:08 -0500

    agregamos configuracion decors para poder hacer el  consumo de el front que realize para el login de prueba

--- index.js
+++ index.js
@@ -5,20 +5,27 @@ const personRoutes = require('./routes/persona.route');
 const moduloRoutes = require('./routes/modulo.route');
 const bitacoraRoutes = require('../BackMejorado/routes/bitacora.route')
 const cors = require('cors');
-
 const app = express();
 
 app.use(express.json()); // Permite manejar datos JSON en las solicitudes
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
 
+dotenv.config();
+
+app.use(cors({
+    origin: 'http://localhost:3001', // Origen permitido (tu frontend)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
+    credentials: true, // Permitir credenciales (ej: cookies, headers de autenticación)
+  }));//permite que cualquier origen acceda a los recursos de la aplicación
+
+  // Manejar solicitudes OPTIONS
+app.options('*', cors()); // Habilita CORS para todas las rutas y métodos
+
 app.use('/api/users', personRoutes);
 app.use('/api/modulos', moduloRoutes);
 app.use('/api/bitacora', bitacoraRoutes);
-
-dotenv.config();
-//app.use(cors({ origin: 'http://192.168.100.7' }));  // Asegúrate de usar http:// si es una URL
-app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
@@ -26,3 +33,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => { // '0.0.0.0' acepta conexiones de cualquier dirección IP
     console.log(`Servidor corriendo en http://192.168.100.7:${PORT}`);
 });
+
