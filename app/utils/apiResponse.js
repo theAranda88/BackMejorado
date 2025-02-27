9245849f71d1c880a011419df162a4e25a2573eb
Daniel Aranda <thearanda88@gmail.com>
2025-02-27 17:08:51 -0500

    last commit add coment in the class api-response

--- app/utils/apiResponse.js
+++ app/utils/apiResponse.js
@@ -2,7 +2,7 @@ class ApiResponse {
     static createApiResponse(message, data) {
       return {
         message,
-        data: Array.isArray(data) ? data : [data]
+        data: Array.isArray(data) ? data : [data] // El item data es un array de datos que pueden ser objetos, valores primitivos, arrays, etc.
       };
     }
   }
