f351ad0cde6a059c7d1eb2af66625fe5c52ee927
Susana Meneses <acuaterraapp@gmail.com>
2025-03-02 22:34:26 -0500

    solve merge conflicts

--- backend/app/services/person.service.js
+++ backend/app/services/person.service.js
@@ -1,22 +1,10 @@
-const { Persona, Rol, Usuario, AdministradorInstructor, Admin } = require('../../models_bck');
+const { Persona, Rol, Usuario, AdministradorInstructor, Admin } = require('../../../models');
 const { Sequelize } = require('sequelize');
 const bcrypt = require('bcrypt');
 const MiddlewareCrearToken = require('../../middleware/CrearToken.Orm');
 const ListaNegraService = require('../services/ListaNegra');
 
 class PersonService {
-
-     async login(password, user) {
-
-            const isPasswordValid = await bcrypt.compare(password, user.password);
-
-            if (!isPasswordValid) {
-                throw Error(`Password not match`);
-            }
-
-            return await MiddlewareCrearToken.CrearToken(user);
-
-    }
 
     static async getAllPersonas() {
         try {
@@ -192,15 +180,6 @@ class PersonService {
         }
     }
 
-    static async logout(token) {
-        try {
-            await ListaNegraService.agregarToken(token);
-            const result =  { message: 'Session closed successfully' };
-            return result;
-        } catch (error) {
-            throw error;
-        }
-    }
 }
 
 module.exports = PersonService;
